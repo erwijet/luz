17f40d92bce9366a846f5c345d05619088c7fc9e
Tyler Holewinski <tyler.holewinski@bryx.com>
2023-02-19 21:47:55 -0500

    Initial Commit

---
+++ luz/luzApp.swift
@@ -0,0 +1,17 @@
+//
+//  luzApp.swift
+//  luz
+//
+//  Created by Tyler Holewinski on 2/19/23.
+//
+
+import SwiftUI
+
+@main
+struct luzApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
